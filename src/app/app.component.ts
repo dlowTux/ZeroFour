26e411164602d6b1089c4bf2afdab809a684f84c
dlowTux <omarlopez22520@gmail.com>
2021-04-28 13:34:18 -0500

    Nueva pagina

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,8 @@ import { faCheckCircle } from '@fortawesome/free-solid-svg-icons';
 import { faUser } from '@fortawesome/free-solid-svg-icons';
 import { faMeh } from '@fortawesome/free-solid-svg-icons';
 import { faBan } from '@fortawesome/free-solid-svg-icons';
+import { faAt } from '@fortawesome/free-solid-svg-icons';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -12,6 +14,7 @@ export class AppComponent {
   title = 'zerofour';
     faOm=faBan;
     faMeh=faMeh;
+    faAt=faAt;
    faCoffee = faCheckCircle; 
     faUser=faUser;
 }
