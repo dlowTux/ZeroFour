e2099765566b8241ea9f8758aeff5d3d502e4fee
dlowTux <omarlopez22520@gmail.com>
2021-04-30 20:26:32 -0500

    Responsibe design

--- src/app/navbard/navbard.component.ts
+++ src/app/navbard/navbard.component.ts
@@ -10,6 +10,11 @@ export class NavbardComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+      const toggleButton=document.getElementsByClassName("toggle-button")[0]
+      const navbardLinks=document.getElementsByClassName("navbar-links")[0]
+      toggleButton.addEventListener('click', ()=>{
+          navbardLinks.classList.toggle("active")
+      })
   }
 
 }
